f19b82e7c0736221fcec64cbe9f984ead72e52f3
Rakib-RS <rakib.rscse@gmail.com>
2019-12-30 20:04:29 +0600

    login router and controller is added

---
+++ server/controllers/adminController.js
@@ -0,0 +1,21 @@
+'use strict'
+const User = require('../models/loginModel');
+const createUser = (req,res)=>{
+    const body = req.body;
+    if(body.email && body.username && body.password){
+        var userData = {
+            email: body.email,
+            username: body.username,
+            password: body.password
+        }
+        User.create(userData,(err,data)=>{
+            return res.json({
+                sucess: true,
+                data: data,
+            })
+        })
+    }
+}
+module.exports ={
+    createUser,
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+var db = require('./db/adminDb');
 const port = process.env.PORT || 8080;
 app.get('/',(req,res)=>{
     res.send("ok server");
