a13d2499faf9e0e5105455c3d992d71307e5425e
BEPUL-CSE-RU <bepul.cse.ru@gmail.com>
2020-01-02 16:28:45 +0600

    Login

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -2,5 +2,4 @@ const adminRouterCtrl = require('../controllers/adminController');
 const express = require('express');
 const router = express.Router();
 router.post('/login',adminRouterCtrl.createUser);
-
 module.exports = router;
