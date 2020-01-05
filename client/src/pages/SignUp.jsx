5b10c91029eab865bdac578bf7a2ffc827cec8ae
Rakib-RS <rakib.rscse@gmail.com>
2020-01-05 17:07:37 +0600

    sign up intregated with back end api now

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,8 +6,45 @@ import api from '../api/adminApi';
         this.state ={
             firstName: '',
             lastName: '',
-            
+            email: '',
+            username: '',
+            password: '',
+
         }
+    }
+    handleChangeFirstName = async(event)=>{
+        const firstName = event.target.value;
+        this.setState({firstName})
+    }
+    handleChangeLastName = async(event)=>{
+        const lastName = event.target.value;
+        this.setState({lastName})
+    }
+    handleChangeEmail = async(event)=>{
+        const email = event.target.value;
+        this.setState({email})
+    }
+    handleChangeUserName = async(event)=>{
+        const username = event.target.value;
+        this.setState({username})
+    }
+    handleChangePassword = async(event)=>{
+        const password = event.target.value;
+        this.setState({password})
+    }
+    handleIncludeAdmin = async() =>{
+        const {firstName,lastName,email,username,password} = this.state;
+        const payload = {firstName,lastName,email,username,password};
+        await api.signup(payload).then((res)=>{
+            window.alert('Signup Succesfully');
+            this.setState({
+                firstName: '',
+                lastName: '',
+                email: '',
+                username: '',
+                password: '',
+            })
+        })
     }
     render() {
         return (
@@ -16,25 +53,41 @@ import api from '../api/adminApi';
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" value={firstName}/>
+                    <input type="text" className="form-control" placeholder="First name" value={firstName} 
+                        onChange = {this.handleChangeFirstName}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" value={lastName} />
+                    <input type="text" className="form-control" placeholder="Last name" value={lastName} 
+                        onChange = {this.handleChangeLastName}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" value={email} />
+                    <input type="email" className="form-control" placeholder="Enter email" value={email} 
+                        onChange = {this.handleChangeEmail}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>User Name</label>
+                    <input type="text" className="form-control" placeholder="Enter UserName" value={username} 
+                        onChange = {this.handleChangeUserName}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" value={password} />
+                    <input type="password" className="form-control" placeholder="Enter password" value={password} 
+                        onChange = {this.handleChangePassword}
+                    />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={this.handleIncludeAdmin}>
+                    Sign Up
+                </button>
                 <p className="forgot-password text-right">
                     Already registered <a href="#">sign in?</a>
                 </p>
