5edd23e6739944d61867591d85f79d17edfa42bc
BEPUL-CSE-RU <bepul.cse.ru@gmail.com>
2020-01-06 15:33:23 +0600

    collapsible

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -14,17 +14,17 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/sign-in"}>RU_Meeting</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                <a className="nav-link" href="/sign-in">Login</a>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
               </li>
             </ul>
           </div>
-        </div>
+      
       </nav>
 
       <div className="auth-wrapper">
