0fcb8723e4d2c1805d59ba36b062ab120090a26d
Rakib-RS <rakib.rscse@gmail.com>
2020-01-06 17:30:22 +0600

    adminlogin.js updated

--- server/controllers/adminLogin.js
+++ server/controllers/adminLogin.js
@@ -2,20 +2,28 @@
 const bcrypt = require('bcryptjs')
 const User = require('../models/loginModel');
 const login = (req,res)=>{
+  console.log(req.body);
   
   const serch = {
     email: req.body.email
   }
 
   User.findOne(serch,(err,user)=>{
+    //console.log(user);
+    
+    if (err || !user) {
+      return res.json({
+        data: "email is not exist"
+      })
+    }
     bcrypt.compare(req.body.password, user.password,  (err, result)=>{
         if(result==true){
-          res.json({
-            success: true
+         return res.json({
+            data: true
           })
         }else{
-          res.json({
-            success: false
+          return res.json({
+            data: "password is incorrect"
           })
         } 
     })
