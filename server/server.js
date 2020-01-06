cd27da14e5e067ad2e1482aa3b7921ce4671f4e9
Rakib-RS <rakib.rscse@gmail.com>
2020-01-06 14:38:14 +0600

    server updated

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var db = require('./db/adminDb');
+const path = require('path');
 const port = process.env.PORT || 8080;
 var router = require('./routes/adminRouter');
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get('/',(req,res)=>{
     res.send("ok server");
 });
+
 app.use('/api',router);
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 app.listen(port,(err)=>{
     console.log('app is listening from '+ port);
 })
