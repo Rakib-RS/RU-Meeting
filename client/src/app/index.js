5de60fdc7c41ad34122509363617856d1ad5140c
Rakib-RS <rakib.rscse@gmail.com>
2020-01-11 18:24:03 +0600

    store.js added

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 //import './App.css';
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {signup,signin} from '../pages/index';
+import { Provider }  from 'react-redux';
+import store from '../store'
 //import Login from '../components/login.component'
 
 
-function App() {
-  return (<Router>
+class App extends Component {
+  render(){
+  return (
+    <Provider store={store}>
+  <Router>
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
@@ -37,7 +42,9 @@ function App() {
         </div>
       </div>
     </div></Router>
+    </Provider>
   );
+  }
 }
 
 
